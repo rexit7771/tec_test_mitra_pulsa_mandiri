01e570000beb2ecde229dd85b81793df8376d205
Muhammad Bagus Pramaskoro <pramaskoro@gmail.com>
2025-05-09 14:47:23 +0700

    Comman /products done

--- soal_3/bot.py
+++ soal_3/bot.py
@@ -3,6 +3,8 @@ from telegram.ext import ApplicationBuilder, CommandHandler
 from dotenv import load_dotenv
 
 from commands.start import start
+from commands.products import products
+
 
 
 load_dotenv()
@@ -12,10 +14,9 @@ def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("products", products))
     
     
-    
-      
 
     app.run_polling()
 
---
+++ soal_3/commands/products.py
@@ -0,0 +1,22 @@
+import requests
+import os
+from telegram import Update
+from telegram.ext import ContextTypes
+
+async def products(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = os.getenv("API_BASE_URL") + "/products"
+    response = requests.get(url)
+    if response.status_code == 200:
+        responseJson = response.json()
+        products = responseJson['data'];
+        msg = ""
+        for index, product in enumerate(products):
+            product_info = f"{index+1}: {product['name']}"
+            if msg:
+                msg += "\n" + product_info
+            else:
+                msg = product_info
+
+        await update.message.reply_text(msg)
+    else:
+        await update.message.repyly_text("Gagal ambil produk")
